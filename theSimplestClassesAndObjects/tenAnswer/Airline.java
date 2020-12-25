d7fe50edbad064c907e6098f60ba7fe195ca8942
Andrei Birukou <medialabs91@gmail.com>
2020-12-25 17:23:50 +0300

    d

---
+++ theSimplestClassesAndObjects/tenAnswer/Airline.java
@@ -0,0 +1,74 @@
+package theSimplestClassesAndObjects.tenAnswer;
+
+
+import java.util.Objects;
+
+public class Airline {
+    private String destination;
+    private int flightNumber;
+    private String aircraftType;
+    private String departureTime;
+    private int dayOfTheWeek;
+
+    public Airline(String destination, int flightNumber, String aircraftType, String departureTime, int dayOfTheWeek) {
+        this.destination = destination;
+        this.flightNumber = flightNumber;
+        this.aircraftType = aircraftType;
+        this.departureTime = departureTime;
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getAircraftType() {
+        return aircraftType;
+    }
+
+    public void setAircraftType(String aircraftType) {
+        this.aircraftType = aircraftType;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getDayOfTheWeek() {
+        return dayOfTheWeek;
+    }
+
+    public void setDayOfTheWeek(int dayOfTheWeek) {
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "destination='" + destination + '\'' +
+                ", flightNumber=" + flightNumber +
+                ", aircraftType='" + aircraftType + '\'' +
+                ", departureTime='" + departureTime + '\'' +
+                ", dayOfTheWeek=" + dayOfTheWeek +
+                '}';
+    }
+
+
+}
