d14244d712133b96b05815f9df2f987e097d7c16
Andrei Birukou <medialabs91@gmail.com>
2020-12-30 13:48:54 +0300

    File reader update

--- io/ReaderMain.java
+++ io/ReaderMain.java
@@ -2,20 +2,32 @@ package io;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ReaderMain {
     public static void main(String[] args) {
 
-        try (FileReader fileReader = new FileReader("data/file.txt")){
+        try (FileReader fileReader = new FileReader("data/file.txt");
+             FileWriter fileWriter = new FileWriter("data/out.txt", true)){
 
-            int simbol = fileReader.read();
-            System.out.println((char) simbol);
+            char[] chars = new char[14];
+            int charNum = fileReader.read(chars);
 
-            char[] chars = new char[8];
-            fileReader.skip(2);
-            fileReader.read(chars);
-            System.out.println(chars);
+            while (charNum != -1) {
+                fileWriter.write(chars, 0, charNum);
+                charNum = fileReader.read(chars);
+            }
+
+
+
+
+//            int simbol = fileReader.read();
+//            System.out.println((char) simbol);
+//            char[] chars = new char[8];
+//            fileReader.skip(2);
+//            fileReader.read(chars);
+//            System.out.println(chars);
         } catch (IOException e) {
             e.printStackTrace();
         }
