68b7129e247ece879a44d0884d0755c82b194b16
Andrei Birukou <medialabs91@gmail.com>
2020-12-30 13:49:12 +0300

    new update

--- io/OutMain.java
+++ io/OutMain.java
@@ -6,8 +6,13 @@ import java.io.IOException;
 public class OutMain {
     public static void main(String[] args) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream("data/out.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream("data/out.txt",true);
+            // if here put flag true file content not be delete
             fileOutputStream.write(48);
+
+            byte[] bytes = {76,87,127,124,};
+
+            fileOutputStream.write(bytes);
         }catch (IOException e){
             e.printStackTrace();
         }
