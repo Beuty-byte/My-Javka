7b8b544d2c9ba2d18e1690106be2c550da6b820b
Andrei Birukou <medialabs91@gmail.com>
2020-12-30 16:20:29 +0300

    some update

--- io/SerializationMain.java
+++ io/SerializationMain.java
@@ -17,5 +17,6 @@ public class SerializationMain {
         }catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
--- io/StudentForSerialization.java
+++ io/StudentForSerialization.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class StudentForSerialization implements Serializable {
     private String name;
     private int id;
-    private String password;
+    private transient String password;
 
     public StudentForSerialization(String name, int id, String password) {
         this.name = name;
